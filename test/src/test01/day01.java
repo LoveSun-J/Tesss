ad983bbaaf0e3339662b52972412f9209f7aadbd
Y <Y@FYYX-2019DJMJLT>
2020-03-07 16:15:08 +0800

    11

--- test/src/test01/day01.java
+++ test/src/test01/day01.java
@@ -3,6 +3,8 @@ package test01;
 public class day01 {
 	public static void main(String[] args) {
 		System.out.println("123456");
+		System.out.println("654321");
+		System.out.println("0987");
 	}
 
 }
